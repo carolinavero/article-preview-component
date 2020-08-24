95b3ad70fbb8866ffa3503050c118735d4b6e2c1
Carolina Vero <carolina@w42.com.br>
2020-08-24 09:58:40 -0300

    styles

--- scripts.js
+++ scripts.js
@@ -2,20 +2,24 @@ let shareBtn = document.querySelector('[data-share]');
 let shareContent = document.querySelector('.footer__share-active');
 let avatarContent = document.querySelector('.footer__avatar');
 let cardFooter = document.querySelector('.card__footer');
+let iconImage = document.querySelector('.icon-share img');
 
 console.log(shareBtn);
 
 shareBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
-     shareContent.classList.toggle('hidden');
+    shareContent.classList.toggle('hidden');
 
-   /*  if(shareContent.hasAttribute('display: flex')) {
-
-    } */
+    if(cardFooter.classList.contains('active')) {
+      shareBtn.style.backgroundColor = 'hsl(210, 46%, 95%)';
+      iconImage.src = 'images/icon-share.svg';
+    } else {
+      shareBtn.style.backgroundColor = '#6f859d';
+      iconImage.src = 'images/icon-share-active.svg';
+    }
 
     avatarContent.classList.toggle('hidden');
-
     cardFooter.classList.toggle('active');
 
 });
